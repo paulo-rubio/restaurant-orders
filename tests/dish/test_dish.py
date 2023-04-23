af89cc7361bc76fe32f94592330e6a5c54692dd9
Rubio <paulocezarrubio@gmail.com>
2023-04-23 10:58:28 -0300

    req1-2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,31 @@
+from src.models.ingredient import Ingredient
 from src.models.dish import Dish  # noqa: F401, E261, E501
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    bolo = Dish('Bolo', 15.00)
+    salgadinho = Dish('salgadinho', 9.00)
+    manteiga = Ingredient('manteiga')
+
+    assert bolo.name == 'Bolo'
+    assert bolo.price == 15
+
+    assert bolo.__eq__(bolo) is True
+    assert bolo.__eq__(salgadinho) is False
+
+    bolo.add_ingredient_dependency(manteiga, 0)
+    assert bolo.get_ingredients() == {
+        Ingredient('manteiga')
+    }
+
+    assert bolo.get_restrictions() == manteiga.restrictions
+
+    assert bolo.__hash__() == hash(bolo.__repr__())
+    assert salgadinho.__hash__() != hash(bolo.__repr__())
+
+    assert bolo.__repr__() == "Dish('Bolo', R$15.00)"
+
+    with pytest.raises(ValueError):
+        Dish('nnada', -100)
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,22 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    bacon = Ingredient('bacon')
+    sorvete = Ingredient('sorvete')
+
+    assert bacon.restrictions == {
+        Restriction.ANIMAL_DERIVED, Restriction.ANIMAL_MEAT
+        }
+
+    assert bacon.__repr__() == "Ingredient('bacon')"
+
+    assert sorvete.__eq__(sorvete) is True
+    assert bacon.__eq__(sorvete) is False
+
+    assert sorvete.name == 'sorvete'
+
+    assert hash('sorvete') == sorvete.__hash__()
+    assert bacon.__hash__() != sorvete.__hash__()
